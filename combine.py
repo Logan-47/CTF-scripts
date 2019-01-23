6b9d12d87d6c3cd3ee4eb1ede74ca57e5ae63d18
Ashish singh <40070226+Logan-47@users.noreply.github.com>
2019-01-23 23:12:02 +0530

    Update combine.py

--- combine.py
+++ combine.py
@@ -7,18 +7,17 @@ start = 1
 last = 22
 counter = 1
 cmd=''
-#print("convert ",end='')
 cmd+='convert '
 for x in range(start,last):
     if x <10:
-        #print (f'1_0{x}.jpg',end=' ')
+       
         cmd+='1_0'+str(x)+'.jpg '
     else:
         cmd+='1_'+str(x)+'.jpg '
 
-#print(f" -append output{counter}.jpg")
+
 cmd+=" +append output"+str(counter)+".jpg"
-#print(cmd)
+
 os.system(cmd)
 counter += 1
 while(last != 442):
@@ -26,23 +25,23 @@ while(last != 442):
     cmd+='convert '
     start = last
     last += 21
-    #print("convert ",end='')
+    
     for x in range(start,last): 
-        #print (f'1_{x}.jpg',end=' ') 
+         
         cmd+='1_'+str(x)+'.jpg '
-   # print(f" -append output{counter}.jpg")
+   
     cmd+=" +append output"+str(counter)+".jpg"
     os.system(cmd)
-   # print(cmd)
+  
     counter += 1
 #final image
 cmd=''
 cmd+='convert '
 for x in range(1,22):
     cmd+='output'+str(x)+'.jpg '
-#print(f" -append output{counter}.jpg")
+
 cmd+=" -append flag.jpg"
-#print(cmd)
+
 os.system(cmd)
 print("check flag.jpg")
 
